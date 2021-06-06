d4a0c065f2044d194518010a47315387d3c4c087
Benjo Kho <benjokho@gmail.com>
2021-06-06 11:54:13 +0800

    Add debug mode

--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+var menu_path := "res://src/menus/MainMenu.tscn"
 func _ready() -> void:
 	pass
 
@@ -11,3 +12,9 @@ func move_board(v) -> void:
 	else:
 		$Blackboard/AnimationPlayer.play_backwards("1to2")
 		
+
+func _unhandled_input(event):
+	if event.is_action_pressed("reset"):
+		var _status = get_tree().reload_current_scene()
+	elif event.is_action_pressed("menu") and menu_path != "":
+		var _status = get_tree().change_scene(menu_path)
